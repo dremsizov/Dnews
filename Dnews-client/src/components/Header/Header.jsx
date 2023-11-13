9ecbfe4a9d2ec9c10246c0ba0e7ba344f2b19c3c
Viktor Dremsizov <viktorkz91@gmail.com>
2023-11-13 17:52:44 +0200

    Css effect to Logo
    I made some logo effect

--- Dnews-client/src/components/Header/Header.jsx
+++ Dnews-client/src/components/Header/Header.jsx
@@ -1,41 +1,42 @@
 
-import Navigation from './Navigation/NavigationBar'
+import Navigation from "./Navigation/NavigationBar";
+import newsLogo from "../../../public/assets/logoMic.png";
+import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
 
-import newsLogo from '../../../public/assets/logoMic.png'
-import styles from './Header.module.css'
-
-import { Link } from "react-router-dom"
 
 
 export default function HeaderComponent() {
-    return(
-      <header>
-        <div className={styles['wrapper']}> 
-        <div className={styles['newsLogo']}>
-        <Link to='/'>
-          <img src={newsLogo} className={styles['logoPicture']} alt="React logo" />
-        </Link>
-        <Link to='/'>
-          <p className={styles['logoText']}>Твоите новини</p>
-        </Link>
-        
+  return (
+    <header>
+      <div className={styles["wrapper"]}>
+        <div className={styles["onOff"]}>
+          <div className={styles["newsLogo"]}>
+            <Link to="/">
+              <img
+                src={newsLogo}
+                className={styles["logoPicture"]}
+                alt="React logo"
+              />
+            </Link>
+            <Link to="/">
+              <p className={styles["logoText"]}>Твоите новини</p>
+            </Link>
+          </div>
+        </div>
+        <div className={styles["icons"]}>
+          <div className={styles["searchBTN"]}>
+            <i className="fa-solid fa-magnifying-glass"></i>
+          </div>
+
+          <div className={styles["login1"]}>
+            <Link to="/login">
+              <i className="fa-solid fa-arrow-right-to-bracket"></i>
+            </Link>
+          </div>
+        </div>
       </div>
-      <div className={styles['icons']}>
-
-        <div className={styles['searchBTN']}>
-          <i className='fa-solid fa-magnifying-glass'></i>
-        </div>
-
-        <div className={styles['login1']}>
-        
-          <Link to="/login"><i className="fa-solid fa-arrow-right-to-bracket"></i>
-          </Link> 
-        </div>
-      </div>
-
-        </div>
-        <Navigation />
-  
-      </header>
-    )
+      <Navigation />
+    </header>
+  );
 }
--- Dnews-client/src/components/Login/Login.jsx
+++ Dnews-client/src/components/Login/Login.jsx
@@ -1,8 +1,34 @@
+import styles from '../Login/Login.module.css'
+
 export default function Login() {
+
 
     return (
         <>
-        <h2>Login Page</h2>
+
+        <section className={styles['loginForm']}>
+        <h2 className={styles['title']}>Влез в своя профил</h2>
+            <form action="">
+                <div className={styles['email']}>
+                <label htmlFor="name">Email</label>
+                <input type="text" />
+                </div>
+                <div className={styles['pass']}>
+                <label htmlFor="name">Password</label>
+                <input type="text" />
+                </div>
+                <div className={styles['signUp']}>
+               
+                <input type="button" value={'LOGIN'} />
+                </div>
+            </form>
+        </section>
+
+        <div className={styles['imgPen']}>
+        <img src="assets/logo.png" alt="pen" />
+
+        </div>
+       
         
         </>
     );
