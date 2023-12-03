87eef9a1e74cdaa6fa5aa380b1ba3b3ae6f09592
Viktor Dremsizov <viktorkz91@gmail.com>
2023-12-03 16:06:16 +0200

    Some Home Css new

--- Dnews-client/src/components/Home/Home.jsx
+++ Dnews-client/src/components/Home/Home.jsx
@@ -1,54 +1,28 @@
 import styles from "../Home/HomePage.module.css";
 
-import * as newsService from "../../services/newsService"
+import * as newsService from "../../services/newsService";
 import { useState, useEffect } from "react";
 
 import HomeNewsCard from "./HomeNewsCard";
 
-// import SliderCard from "../SliderCard/SliderCard";
-
-
 
 export default function Home() {
+  const [newsSeven, setNews] = useState([]);
 
-
-  const [allNews,setAllNews] = useState([]);
-
-
-    useEffect(() => {
-        newsService.getAll()
-       .then(result =>setAllNews(result))
-       .catch(error => console.log(error));
-
-    }, []);
-
-    const reversedNews = [...allNews].reverse();
-    
+  useEffect(() => {
+    newsService
+      .getLastSevenNews()
+      .then((result) => setNews(result))
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
-    
-   
-    
     <>
-     {/* <div className={styles.newsContainer}>
-            {reversedNews.map(newscard =>(
-                <SliderCard 
-                {...newscard}
-                />
-                ))}
-        </div> */}
-
-    <div className={styles.newsContainer}>
-            {reversedNews.map(newscard =>(
-                <HomeNewsCard 
-                {...newscard}
-                />
-                ))}
-        </div>
+      <div className={styles.newsContainer}>
+        {newsSeven.map((newscard) => (
+          <HomeNewsCard {...newscard} />
+        ))}
+      </div>
     </>
-       
-        
-      
-    
   );
 }
