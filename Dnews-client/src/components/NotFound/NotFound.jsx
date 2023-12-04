04cc38487f8568ad30fe705c44a0ada6a6ac3569
Viktor Dremsizov <viktorkz91@gmail.com>
2023-12-04 15:06:20 +0200

    Title tab add

--- Dnews-client/src/components/NotFound/NotFound.jsx
+++ Dnews-client/src/components/NotFound/NotFound.jsx
@@ -1,6 +1,7 @@
 import styles from '../NotFound/NotFound.module.css'
 
 export default function NotFound(){
+    document.title = 'Грешка';
     return(
         <>
         <div className={styles['image']}>
