f6a9195719fb28dbe8a1276ba51a1335cd77eae0
Viktor Dremsizov <viktorkz91@gmail.com>
2023-12-06 16:26:14 +0200

    Spinner

--- Dnews-client/src/components/Home/Home.jsx
+++ Dnews-client/src/components/Home/Home.jsx
@@ -1,28 +1,61 @@
+/* eslint-disable no-unused-vars */
 import styles from "../Home/HomePage.module.css";
 
 import * as newsService from "../../services/newsService";
 import { useState, useEffect } from "react";
 
 import HomeNewsCard from "./HomeNewsCard";
+import Spiner from "../SPINER/Spiner";
+
+
+
 
 
 export default function Home() {
   const [newsSeven, setNews] = useState([]);
+  const [spining, setSpining] = useState(false);
+  const[hasServerError, setHasServerError]= useState(false);
+  const[serverError,setServerError]= useState({})
 
   useEffect(() => {
+    setSpining(true);
     newsService
       .getLastSevenNews()
       .then((result) => setNews(result))
-      .catch((err) => console.log(err));
+      .catch( error => {
+        setHasServerError(true);
+        setServerError(error.message)
+        console.log(error.message);
+      })
+      .finally(()=> setSpining(false));        
+                
   }, []);
 
   return (
     <>
-      <div className={styles.newsContainer}>
-        {newsSeven.map((newscard) => (
-          <HomeNewsCard key={newscard._id} {...newscard} />
-        ))}
-      </div>
+    {spining && <Spiner />}
+            {hasServerError && (
+                        <p className={styles.serverError}>Грешка!( </p>
+                    )}
+
+              {newsSeven.length >0 
+              ? (
+                <>
+          <div className={styles.newsContainer}>
+              {newsSeven.map((newscard) => (
+                 <HomeNewsCard key={newscard._id} {...newscard} />
+                 ))}
+          </div>
+                </>
+
+              )
+              :
+              <div className={styles.NoNewsContainer}>
+                            <p className={styles.NoNews}>Няма новини за показване!</p>
+                        </div>
+              
+              }
+      
     </>
   );
 }
